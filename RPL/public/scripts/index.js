e62e6b69bd6a84a9011f7947c62701c6d520e6cb
LeevioP <95002649+LeevioP@users.noreply.github.com>
2023-06-17 14:40:40 +0800

    Add files via upload

---
+++ RPL/public/scripts/index.js
@@ -0,0 +1,56 @@
+const loginElement = document.querySelector('#login-form');
+const contentElement = document.querySelector("#content-sign-in");
+const userDetailsElement = document.querySelector('#user-details');
+const authBarElement = document.querySelector("#authentication-bar");
+
+// Elements for sensor readings
+const tempElement = document.getElementById("temp");
+const humElement = document.getElementById("hum");
+const rainElement = document.getElementById("rain");
+
+// MANAGE LOGIN/LOGOUT UI
+const setupUI = (user) => {
+  if (user) {
+    //toggle UI elements
+    loginElement.style.display = 'none';
+    contentElement.style.display = 'block';
+    authBarElement.style.display ='block';
+    userDetailsElement.style.display ='block';
+    userDetailsElement.innerHTML = user.email;
+
+    // get user UID to get data from database
+    var uid = user.uid;
+    console.log(uid);
+
+    // Database paths (with user UID)
+    var dbPathTemp = 'UsersData/' + uid.toString() + '/temperature';
+    var dbPathHum = 'UsersData/' + uid.toString() + '/humidity';
+    var dbPathRain = 'UsersData/' + uid.toString() + '/rainfall';
+
+    // Database references
+    var dbRefTemp = firebase.database().ref().child(dbPathTemp);
+    var dbRefHum = firebase.database().ref().child(dbPathHum);
+    var dbRefRain = firebase.database().ref().child(dbPathRain);
+
+    // Update page with new readings
+    dbRefTemp.on('value', snap => {
+      tempElement.innerText = snap.val().toFixed(2);
+    });
+
+    dbRefHum.on('value', snap => {
+      humElement.innerText = snap.val().toFixed(2);
+    });
+
+    dbRefRain.on('value', snap => {
+      rainElement.innerText = snap.val().toFixed(2);
+    });
+
+  // if user is logged out
+  } else{
+    // toggle UI elements
+    loginElement.style.display = 'block';
+    authBarElement.style.display ='none';
+    userDetailsElement.style.display ='none';
+    contentElement.style.display = 'none';
+  }
+}
